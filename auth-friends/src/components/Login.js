fc07749c4771604dc27ec08b09e7df50ee133180
Wangdi Tenzing <wangdi457@gmail.com>
2020-02-16 19:39:49 -0500

    Added a login Component

---
+++ auth-friends/src/components/Login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function LogIn() {
+  return (
+    <div className="LogIn">
+     <form>
+         <input 
+         type="text"
+         placeholder="username"
+        //  onChange={handleSumbit}
+         
+          />
+
+<input 
+         type="text"
+         placeholder="password"
+        //  onChange={handleSumbit}
+         
+          />
+
+
+         
+     </form>
+    </div>
+  );
+}
+
+export default LogIn;
