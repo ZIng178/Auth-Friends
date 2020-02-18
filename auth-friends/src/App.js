737f987509b19b712b0ef3b041d5f0dd18f207d3
Wangdi Tenzing <wangdi457@gmail.com>
2020-02-17 20:23:33 -0500

    Added functionality to the login component

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-import Login from "../src/components/Login";
-
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+// import { axiosWithAuth } from "./components/AxiosWithAuth";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import FriendsList from "./components/FriendsList";
 function App() {
   return (
-    <div className="App">
-     <Login/>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/login"> LogIn</Link>
+          {/* <Link to="/protected"> Protected Page</Link> */}
+        </nav>
+        <Route exact path="/login" component={Login} />
+        {/* <ProtectedRoute exact path="/protected" component={FriendsList} />> */}
+      </div>
+    </Router>
   );
 }
 
--- auth-friends/src/components/Login.js
+++ auth-friends/src/components/Login.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function LogIn() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("submitting");
+  };
+
   return (
     <div className="LogIn">
-     <form>
-         <input 
-         type="text"
-         placeholder="username"
-        //  onChange={handleSumbit}
-         
-          />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+          value={credentials.username}
+        />
 
-<input 
-         type="text"
-         placeholder="password"
-        //  onChange={handleSumbit}
-         
-          />
-
-
-         
-     </form>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+          value={credentials.password}
+        />
+     
+      <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
 
 export default LogIn;
+
+
